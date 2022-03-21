54814e15d399546e1e8ae8f7769eff53bae60ba7
Mulao Mudau <Mudau.Mulalo@gmail.com>
2022-03-21 23:27:46 +0200

    Initial commit

---
+++ CurrencyConverter/Models/Currency.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CurrencyConverter.Models
+{
+    public class Currency
+    {
+        public int Id { get; set; }
+        public string currencyFrom { get; set; }
+        public string currencyTo { get; set; }
+        public double amountFrom { get; set; }
+        public double amountTo { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
